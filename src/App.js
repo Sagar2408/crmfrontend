2bc731ac31d2cb3ab9875d0a01111e5121b93367
Sagar <sagar90160@gmail.com>
2025-06-03 16:00:17 +0530

    dialer system integration

--- src/App.js
+++ src/App.js
@@ -35,7 +35,8 @@ import ExecutiveFormRoutes from "./layouts/ExecutiveFormRoutes";
 
 // 🆕 Global Dialer Context and UI
 import { CallProvider } from "./context/CallContext";
-import DialerBox from "./features/DialerBox";
+import DialerBox from "./features/dialerbox/dialerbox";
+
 
 const App = () => {
   const [followUpText, setFollowUpText] = useState(() => {
