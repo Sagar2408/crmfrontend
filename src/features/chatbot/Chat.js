2afc17f7a8771683cdeddb5ca84d05248ed8a7f0
Sagar <sagar90160@gmail.com>
2025-06-09 16:20:08 +0530

    monday work

--- src/features/chatbot/Chat.js
+++ src/features/chatbot/Chat.js
@@ -92,7 +92,7 @@ const Chat = ({ isCallActive }) => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "x-company-id": "aba4db75-437f-11f0-a807-a2aaa2e2ab32"
+          "x-company-id": "f515cb0e-450f-11f0-bcd7-a2aa1a8f1119"
         },
         body: JSON.stringify({ prompt: input }),
       });
